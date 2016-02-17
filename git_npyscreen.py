0b29b2b2c99d56dc08395d268ff0406b02518f3c
Matthew Burke <matthew.burke@cantab.net>
2016-02-17 16:17:57 +0000

    added commit to alternative branch

--- git_npyscreen.py
+++ git_npyscreen.py
@@ -18,8 +18,47 @@ class repo_multiline(npyscreen.MultiLine):
 class stage_multiselect(npyscreen.MultiSelect):
     pass
 
-class commit_selectone(npyscreen.SelectOne):
+class commit_multiselect(npyscreen.MultiSelect):
     pass
+
+class branch_selectone(npyscreen.SelectOne):
+    pass
+
+#class MergeForm(npyscreen.ActionForm):
+#    def create(self,*args,**keywords):
+#        self.name = "Choose branch to merge with:"
+#        self.repo_name = ''
+#        self.repo_path = ''
+#        self.branch_list = []
+#        self.merge_selectone = self.add(merge_selectone,name='merge',values=[])
+#    #def set_up_handlers(self):
+#        #super(EditForm,self).set_up_handlers()
+#        #self.handlers.update({})
+#    def on_cancel(self):
+#        self.parentApp.switchFormPrevious()
+#    def on_ok(self):
+#        #self.parentApp.getForm('STAGE').name = self.commit_message.value
+#        git_utils.commit_files(self.repo_path,self.file_list,self.commit_message.value)
+#        #git_utils.commit_files(self.repo_path,self.file_list,self.commit_selectone.values[self.commit_selectone.cursor_line][0])
+#        self.parentApp.switchFormPrevious()
+ 
+#class CommitListForm(npyscreen.ActionForm):
+#    def create(self,*args,**keywords):
+#        self.name = 'List of commits not yet merged:'
+#        self.repo_name = ''
+#        self.repo_path = ''
+#        self.commit_multiselect = self.add(commit_multiselect,name='commit',value=0)
+#    def on_cancel(self):
+#        self.parentApp.setNextFormPrevious()
+#    def on_ok(self):
+#        commit_list = []
+#        for commit in self.commit_multiselect.get_selected_objects():
+#            commit_list.append(commit)
+#        self.parentApp.getForm('MERGE').merge_selectone.values=git_utils.get_branches(self.repo_path)
+#        self.parentApp.getForm('MERGE').repo_path = self.repo_path
+#        self.parentApp.getForm('MERGE').repo_name = self.repo_name
+#        self.parentApp.getForm('MERGE').file_list = commit_list
+#        self.parentApp.setNextForm('MERGE')
 
 class CommitForm(npyscreen.ActionForm):
     def create(self,*args,**keywords):
@@ -27,16 +66,25 @@ class CommitForm(npyscreen.ActionForm):
         self.repo_name = ''
         self.repo_path = ''
         self.file_list = ''
+        self.get_branch_name = 'master'
         self.commit_message = self.add(npyscreen.TitleText,name='Commit message: ')
-        #self.commit_selectone = self.add(commit_selectone,name='commit',value=0)
+        self.branch_list = []
+        self.add(npyscreen.TitleText,value="The current branch is selected. If this commit is intended for another branch specify it here. In that case I'll do the commit but then perform a git reset HEAD~1 and roll back the working tree and index. Then you can pick up the pieces from git reflog.")
+        self.branch_selectone = self.add(branch_selectone,name='merge',value=0,values=[])
+    #def set_up_handlers(self):
+        #super(EditForm,self).set_up_handlers()
+        #self.handlers.update({"m": self.merge})
     def on_cancel(self):
         self.parentApp.switchFormPrevious()
     def on_ok(self):
         #self.parentApp.getForm('STAGE').name = self.commit_message.value
-        git_utils.commit_files(self.repo_path,self.file_list,self.commit_message.value)
+        commit_branch_name = ''
+        for branch in self.branch_selectone.get_selected_objects():
+            commit_branch_name = branch
+        git_utils.commit_files(self.repo_path,self.file_list,self.commit_message.value,commit_branch_name)
         #git_utils.commit_files(self.repo_path,self.file_list,self.commit_selectone.values[self.commit_selectone.cursor_line][0])
         self.parentApp.switchFormPrevious()
-
+    
 class EditForm(npyscreen.Form):
     def create(self,*args,**keywords):
         self.name = "Add Repo"
@@ -73,6 +121,8 @@ class StageForm(npyscreen.ActionForm):
         self.parentApp.getForm('COMMIT').repo_path = self.repo_path
         self.parentApp.getForm('COMMIT').repo_name = self.repo_name
         self.parentApp.getForm('COMMIT').file_list = file_list
+        self.parentApp.getForm('COMMIT').branch_selectone.values = git_utils.get_branches(self.repo_path)
+        self.parentApp.getForm('COMMIT').branch_selectone.value = git_utils.get_current_branch_number(self.repo_path)
         self.parentApp.setNextForm('COMMIT')
 
 class MainForm(npyscreen.ActionForm):
@@ -81,7 +131,7 @@ class MainForm(npyscreen.ActionForm):
         self.repo_multiline = self.add(repo_multiline,name='repos',values=sqlite_utils.list_repos(),value=0)
     def set_up_handlers(self):
         super(MainForm,self).set_up_handlers()
-        self.handlers.update({"q": self.exit,'a': self.edit_form,'s': self.stage})
+        self.handlers.update({"m": self.merge,"q": self.exit,'a': self.edit_form,'s': self.stage})
     #def afterEditing(self):
         #self.parentApp.setNextForm(None)
     def stage(self,input):
@@ -96,6 +146,11 @@ class MainForm(npyscreen.ActionForm):
         self.parentApp.switchForm(None)
     def edit_form(self,input):
         self.parentApp.switchForm('EDIT')
+    def merge(self,*args,**keywords):
+        self.parentApp.getForm('COMMITLIST').commit_multiselect.values = git_utils.get_commits(self.repo_multiline.values[self.repo_multiline.cursor_line][1])
+        self.parentApp.getForm('COMMITLIST').repo_name = self.repo_multiline.values[self.repo_multiline.cursor_line][0]
+        self.parentApp.getForm('COMMITLIST').repo_path = self.repo_multiline.values[self.repo_multiline.cursor_line][1]
+        self.parentApp.switchForm('COMMITLIST')
 
 if __name__ == '__main__':
     TA = MyTestApp()
--- git_utils.py
+++ git_utils.py
@@ -3,10 +3,32 @@ from sys import argv
 import os
 import datetime
 
-def commit_files(repo_path,file_list,commit_message):
+def get_current_branch_number(repo_path):
+    repo = Repo(repo_path)
+    value = 0
+    branch_list = []
+    for param, head in enumerate(repo.heads):
+        if head.name == repo.active_branch.name:
+            value = param
+    print value
+    return value
+
+def get_branches(repo_path):
+    repo = Repo(repo_path)
+    branch_list = []
+    for head in repo.heads:
+        branch_list.append(head.name)
+    print branch_list
+    return branch_list
+
+def commit_files(repo_path,file_list,commit_message,commit_branch_name):
     repo = Repo(repo_path)
     repo.index.add(file_list)
-    repo.index.commit(commit_message)
+    if commit_branch_name == repo.active_branch.name:
+        repo.index.commit(commit_message)
+    else:
+        repo.index.commit(commit_message+'INTENDED BRANCH='+commit_branch_name)
+        repo.head.reset('HEAD~1',working_tree=True,index=True)
 
 def get_modified_files(repo_path):
     repo = Repo(repo_path)
@@ -45,3 +67,4 @@ if __name__ == '__main__':
     get_staged_files(repo_path)
     get_commits(repo_path)
     repo_last_fetch_time(repo_path)
+    get_branches(repo_path)
