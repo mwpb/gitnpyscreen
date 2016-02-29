f099ba52e32e3400d10dae77dd8a9c4bfa309187
Matthew Burke <matthew.burke@cantab.net>
2016-02-29 12:56:13 +0000

    added new files

---
+++ main_form.py
@@ -0,0 +1,75 @@
+import npyscreen
+import sqlite_utils
+import git_utils
+
+class repo_multiline(npyscreen.MultiLine):
+    def display_value(self,vl):
+        return "{:15} {:3}{:19} {:30} {:9} {:7}".format(vl[0],vl[5],vl[2],vl[1],vl[3],vl[4])
+
+class bindings_pager(npyscreen.Pager):
+    pass
+
+class MainForm(npyscreen.ActionForm):
+    def create(self,*args,**keywords):
+        self.name = "Repos"
+        self.bindings_string='''
+        u - stage Untracked files
+        t - stage Tracked files
+        m - merge tracked branch with local or remote
+        a - push files that are Ahead of remote (displays origin master at moment)
+        + - add repo
+        - - remove repo under cursor
+        '''
+        self.bindings_list = self.bindings_string.split('\n')
+        self.add_widget(npyscreen.FixedText,editable=False,color='green',value="{:15} {:3}{:19} {:30} {:9} {:7}".format('Repo Name','(B)','Last Fetch','Repo Path','Tracking','u/t/m/a'))
+        self.repo_multiline = self.add(repo_multiline,name="repos",values=sqlite_utils.list_repos(),value=0,max_height=10)
+        self.bindings_pager = self.add(bindings_pager,name='bindings',values=self.bindings_list)
+    def beforeEditing(self):
+        self.repo_multiline.values = sqlite_utils.list_repos()
+    def set_up_handlers(self):
+        super(MainForm,self).set_up_handlers()
+        self.handlers.update({'u':self.untracked_files,'-':self.remove_repo,'c':self.checkout,'a':self.do_push,'r':self.on_refresh,"f":self.fetch,"m": self.merge,"q": self.exit,'+': self.edit_form,'t':self.stage})
+    #def afterEditing(self):
+        #self.parentApp.setNextForm(None)
+    def stage(self,input):
+        self.parentApp.getForm('STAGE').name = "Staging area for %s" % str(self.repo_multiline.values[self.repo_multiline.cursor_line][0])
+        self.parentApp.getForm('STAGE').repo_name = self.repo_multiline.values[self.repo_multiline.cursor_line][0]
+        self.parentApp.getForm('STAGE').repo_path = self.repo_multiline.values[self.repo_multiline.cursor_line][1]
+        self.parentApp.getForm('STAGE').stage_multiselect.values=sqlite_utils.get_modified_files(str(self.repo_multiline.values[self.repo_multiline.cursor_line][1]))
+        self.parentApp.switchForm('STAGE')
+    def on_ok(self):
+        self.parentApp.switchForm(None)
+    def exit(self,input):
+        self.parentApp.switchForm(None)
+    def edit_form(self,input):
+        self.parentApp.switchForm('EDIT')
+    def merge(self,*args,**keywords):
+        self.parentApp.getForm('MERGE').merge_selectone.values = git_utils.get_branches(self.repo_multiline.values[self.repo_multiline.cursor_line][1])+git_utils.get_remote_branches(self.repo_multiline.values[self.repo_multiline.cursor_line][1])
+        self.parentApp.getForm('MERGE').repo_name = self.repo_multiline.values[self.repo_multiline.cursor_line][0]
+        self.parentApp.getForm('MERGE').repo_path = self.repo_multiline.values[self.repo_multiline.cursor_line][1]
+        self.parentApp.switchForm('MERGE')
+    def fetch(self,*args,**keywords):
+        git_utils.git_fetch(self.repo_multiline.values[self.repo_multiline.cursor_line][1])
+        self.repo_multiline.values = sqlite_utils.list_repos()
+        self.repo_multiline.display()
+        self.DISPLAY()
+    def on_refresh(self,*args,**keywords):
+        self.repo_multiline.display()
+        self.DISPLAY()
+    def do_push(self,*args,**keywords):
+        self.parentApp.getForm('REMOTES').repo_path = self.repo_multiline.values[self.repo_multiline.cursor_line][1]
+        self.parentApp.getForm('REMOTES').remote_selectone.values = git_utils.get_remote_branches(self.repo_multiline.values[self.repo_multiline.cursor_line][1])
+        self.parentApp.switchForm('REMOTES')        
+    def checkout(self,*args,**keywords):
+        self.parentApp.getForm('CHECKOUT').repo_path = self.repo_multiline.values[self.repo_multiline.cursor_line][1]
+        self.parentApp.getForm('CHECKOUT').checkout_selectone.values = git_utils.get_branches(self.repo_multiline.values[self.repo_multiline.cursor_line][1])
+        self.parentApp.switchForm('CHECKOUT')        
+    def remove_repo(self,*args,**keywords):
+        sqlite_utils.delete_repo(self.repo_multiline.values[self.repo_multiline.cursor_line][0])
+        self.beforeEditing()
+        self.repo_multiline.display()
+        self.DISPLAY()
+    def untracked_files(self,*args,**keywords):
+        self.parentApp.getForm('UNTRACKED').untracked_multiselect.values = git_utils.untracked_files(self.repo_multiline.values[self.repo_multiline.cursor_line][1])
+        self.parentApp.getForm('UNTRACKED').repo_path = self.repo_multiline.values[self.repo_multiline.cursor_line][1]
+        self.parentApp.switchForm('UNTRACKED')
