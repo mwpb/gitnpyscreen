47bc62d9c2e6928fcf2c382c991d694b90d9bf12
Matthew Burke <matthew.burke@cantab.net>
2016-02-29 12:56:13 +0000

    added new files

---
+++ remote_form.py
@@ -0,0 +1,17 @@
+import npyscreen
+import sqlite_utils
+import git_utils
+
+class remote_selectone(npyscreen.SelectOne):
+    pass 
+
+class RemoteForm(npyscreen.ActionForm):
+    def create(self,*args,**keywords):
+        self.repo_path = ''
+        self.name = 'Choose remote:'
+        self.remote_selectone = self.add(remote_selectone,name='remotes',values=[])
+    def on_cancel(self):
+        self.parentApp.switchFormPrevious()
+    def on_ok(self):
+        git_utils.push_remote(self.repo_path,self.remote_selectone.get_selected_objects())
+        self.parentApp.switchFormPrevious()
