8f1e0cd477e2f11073b54d1bbaa0e7d5c9146f72
Matthew Burke <matthew.burke@cantab.net>
2016-04-01 12:59:05 +0100

    comment added

--- commit_form.py
+++ commit_form.py
@@ -19,6 +19,7 @@ class CommitForm(npyscreen.ActionForm):
         self.parentApp.switchFormPrevious()
     def on_ok(self):
         if git_utils.tracked_branch(self.active_branch,self.active_branch) != None:
+            #comment
             self.parentApp.getForm('MAIN').name = 'self.active_branch'
             temp_branch = self.active_branch+'-tmp'
             git_utils.create_branch(self.repo_path,temp_branch)
