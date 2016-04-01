34e4dcb5c28904c74540575119ccc561f9d35df3
Matthew Burke <matthew.burke@cantab.net>
2016-04-01 15:24:50 +0100

    included branch name in pull

--- git_utils.py
+++ git_utils.py
@@ -164,7 +164,7 @@ def start_branch_track(repo_path,local_branch,remote_branch):
 
 def rebase(repo_path,branch_name):
     git = sh.git.bake(_cwd=repo_path)
-    git('pull','--rebase','origin')
+    git('pull','--rebase','origin',branch_name)
     git('rebase',branch_name)
     git('checkout',branch_name)
     git('merge',branch_name+'-tmp')
