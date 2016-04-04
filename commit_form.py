32963bb489890cee020374d74a9bf87588359ee6
Matthew Burke <matthew.burke@cantab.net>
2016-04-04 11:25:07 +0100

    commit form shortened

--- commit_form.py
+++ commit_form.py
@@ -1,18 +1,19 @@
 import npyscreen
 import sqlite_utils
 import git_utils
-
-class commit_multiselect(npyscreen.MultiSelect):
-    pass
+import curses
 
 class CommitForm(npyscreen.ActionForm):
     def create(self,*args,**keywords):
-        self.name = "Commit message and branch confirmation"
+        self.name = "Commit message"
         self.repo_name = ''
         self.repo_path = ''
         self.file_list = ''
         self.active_branch = 'master'
         self.commit_message = self.add_widget(npyscreen.TitleText,use_two_lines=False,name='Commit message:')
+    def set_up_handlers(self):
+        super(CommitForm,self).set_up_handlers()
+        self.handlers.update({curses.KEY_ENTER:self.on_ok,"q": self.on_cancel})
     def beforeEditing(self):
         self.active_branch = git_utils.active_branch(self.repo_path)
     def on_cancel(self):
--- git_utils.py
+++ git_utils.py
@@ -5,83 +5,83 @@ import os
 import sh
 import datetime
 
-#def track_files(repo_path,file_list):
-#    git = sh.git.bake(_cwd=repo_path)
-#    for file_for_tracking in file_list:
-#        git.add(file_for_tracking)
-#
-#def checkout(repo_path,new_branch):
-#    git = sh.git.bake(_cwd=repo_path)
-#    git.checkout(new_branch)
-#
-#def git_fetch(repo_path):
-#    git = sh.git.bake(_cwd=repo_path)
-#    git.fetch('-p')
-#
-#def get_active_branch(repo_path):
-#    git = sh.git.bake(_cwd=repo_path)
-#    active_branch = git('rev-parse','--abbrev-ref','HEAD')
-#    #print repo.active_branch.name
-#    return active_branch
-#
-#def merge_current(repo_path,branch_name):
-#    git = sh.git.bake(_cwd=repo_path)
-#    git.merge(branch_name)
-#
-#def get_current_branch_number(repo_path):
-#    repo = Repo(repo_path)
-#    value = 0
-#    branch_list = []
-#    for param, head in enumerate(repo.heads):
-#        if head.name == repo.active_branch.name:
-#            value = param
-#    #print value
-#    return value
-#
-#def get_branches(repo_path):
-#    repo = Repo(repo_path)
-#    branch_list = []
-#    for head in repo.heads:
-#        branch_list.append(head.name)
-#    #print branch_list
-#    return branch_list
-#
-#def get_staged_files(repo_path):
-#    git = sh.git.bake(_cwd=repo_path)
-#    repo = Repo(repo_path)
-#    staged_files = []
-#    for file in repo.index.diff('Head'):
-#        lines = str(file).split('\n')
-#        staged_files.append(lines[0])
-#    #print staged_files
-#    return staged_files
-#
-#def get_commits(repo_path):
-#    repo = Repo(repo_path)
-#    commits_ahead = []
-#    for commit in repo.iter_commits('origin/master..master'):
-#        commits_ahead.append(commit.message)
-#    #print commits_ahead
-#    return commits_ahead
-#
-#def get_commits_behind(repo_path):
-#    repo = Repo(repo_path)
-#    commits_behind = []
-#    for commit in repo.iter_commits('master..origin/master'):
-#        commits_behind.append(commit.message)
-#    #print commits_behind
-#    return commits_behind
-#
-#def repo_last_fetch_time(repo_path):
-#    try:
-#        t = os.path.getmtime(repo_path+'.git/FETCH_HEAD')
-#        #print datetime.datetime.fromtimestamp(t)
-#        return datetime.datetime.fromtimestamp(t)
-#    except:
-#        #print 'No FETCH_HEAD'
-#        return 'No FETCH_HEAD'
-#
-## Rebase workflow completely contained below
+def track_files(repo_path,file_list):
+    git = sh.git.bake(_cwd=repo_path)
+    for file_for_tracking in file_list:
+        git.add(file_for_tracking)
+
+def checkout(repo_path,new_branch):
+    git = sh.git.bake(_cwd=repo_path)
+    git.checkout(new_branch)
+
+def git_fetch(repo_path):
+    git = sh.git.bake(_cwd=repo_path)
+    git.fetch('-p')
+
+def get_active_branch(repo_path):
+    git = sh.git.bake(_cwd=repo_path)
+    active_branch = git('rev-parse','--abbrev-ref','HEAD')
+    #print repo.active_branch.name
+    return active_branch
+
+def merge_current(repo_path,branch_name):
+    git = sh.git.bake(_cwd=repo_path)
+    git.merge(branch_name)
+
+def get_current_branch_number(repo_path):
+    repo = Repo(repo_path)
+    value = 0
+    branch_list = []
+    for param, head in enumerate(repo.heads):
+        if head.name == repo.active_branch.name:
+            value = param
+    #print value
+    return value
+
+def get_branches(repo_path):
+    repo = Repo(repo_path)
+    branch_list = []
+    for head in repo.heads:
+        branch_list.append(head.name)
+    #print branch_list
+    return branch_list
+
+def get_staged_files(repo_path):
+    git = sh.git.bake(_cwd=repo_path)
+    repo = Repo(repo_path)
+    staged_files = []
+    for file in repo.index.diff('Head'):
+        lines = str(file).split('\n')
+        staged_files.append(lines[0])
+    #print staged_files
+    return staged_files
+
+def get_commits(repo_path):
+    repo = Repo(repo_path)
+    commits_ahead = []
+    for commit in repo.iter_commits('origin/master..master'):
+        commits_ahead.append(commit.message)
+    #print commits_ahead
+    return commits_ahead
+
+def get_commits_behind(repo_path):
+    repo = Repo(repo_path)
+    commits_behind = []
+    for commit in repo.iter_commits('master..origin/master'):
+        commits_behind.append(commit.message)
+    #print commits_behind
+    return commits_behind
+
+def repo_last_fetch_time(repo_path):
+    try:
+        t = os.path.getmtime(repo_path+'.git/FETCH_HEAD')
+        #print datetime.datetime.fromtimestamp(t)
+        return datetime.datetime.fromtimestamp(t)
+    except:
+        #print 'No FETCH_HEAD'
+        return 'No FETCH_HEAD'
+
+# Rebase workflow completely contained below
 def push_remote(repo_path,remote_name):
     (remote,branch) = remote_name.split('/')
     git = sh.git.bake(_cwd=repo_path)
